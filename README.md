3b2807087b6707bc55d9a420ac23aebbd2fff53e
Raul Viana <raulmanuelviana@gmail.com>
2019-03-13 19:38:51 +0000

    Added processFile.c and processFile

--- README.md
+++
@@ -1,4 +0,0 @@
-# Feup.SOPE.TG1
-Repository for SOPE course's first group work 
-
-Group work objective is to create a file and diretories forensic analysis tool.
---
+++ processFile.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char *argv[], char *envp[]){
+
+
+printf("I'm %s\n", argv[0]);
+
+
+
+    exit(0);
+}
