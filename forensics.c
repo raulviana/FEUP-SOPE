7d99e7e17047abd97f6e17659cedb6d903cc0712
Raul Viana <raulmanuelviana@gmail.com>
2019-04-03 17:02:15 +0100

    parsing directory recursively almost done

--- forensics.c
+++ forensics.c
@@ -8,6 +8,7 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdbool.h>
+#include <wait.h>
 
 #define READ 0
 #define WRITE 1
@@ -15,7 +16,7 @@
 
 
 //Chama o processo externo "file" para obter o tipo de ficheiro
-char getFileInfo(char* filename, char filetype[], char* filePath, char* file){
+char getFileInfo(char* filenamePtr, char filetype[], char* filePathPtr, char* filePtr){
 
 int fd1[2], fd2[2], n;
 pid_t pid;
@@ -23,6 +24,13 @@ pipe(fd1); pipe(fd2);
 char result[MAXLINE];
 const char token[2] = ":";
 const char* ptr;
+char filename[512];     //File to be procesed by the co-process
+strcpy(filename, filenamePtr);
+char filePath[512];    //Co-Proces path to be called 
+strcpy(filePath, filePathPtr);
+char file[512];       //Co-Process name to be called
+strcpy(file, filePath);   
+
 
 pid = fork();
 if(pid > 0){
@@ -33,7 +41,7 @@ if(pid > 0){
   result[n] = 0;
 	int i = 0;
 	while(result[i]!='\0') {
-      if(result[i]==',') {
+      if(result[i]==',') { //Se result contiver ',' sera substituido por '|'
         result[i]='|';
         }
         i++;
@@ -60,10 +68,26 @@ else{
 }
 }
 
+//Get the file name from file path
+//--------------------------------
+void cleanName(char name[]){
+	char temp[512];
+	char* ptr;
+	ptr = strtok(name, "/");
+	while (ptr != NULL){
+		
+		strcpy(temp, ptr);
+		ptr = strtok(NULL, "/");
+		if(ptr == NULL){
+			strcpy(name, temp);
+		}
+	}
+}
+
 //Imprime caracteristicas do ficheiro
 //_____________________________________
-
-void printFileInfo(char* filename, int cripto) {
+void printFileInfo(char filenamePtr[], int cripto, char outFilePathPtr[]) {
+  
 	struct stat file_info;
 	char time_buffer[80];
 	char filetype[80];
@@ -78,16 +102,39 @@ void printFileInfo(char* filename, int cripto) {
 	char sha256[] = "sha256sum";
 	char sha256Path[] = "/usr/bin/sha256sum";
 	char sha256result[80];
-
-	getFileInfo(filename, filetype, filePath, file);
-	strtok(filetype, "\n");
+  char filename[256];
+	int fd_out;
+	strcpy(filename, filenamePtr);
 	
-	printf("%s", filename);
-	printf(",");
+	getFileInfo(filename, filetype, filePath, file);
+	strtok(filetype, "\n");  //retira o newline
+
+	// Changes the output to te specified file
+	//------------------------------------------
+	if (0) { //argument "-o" present
+	
+	char outFilePath[256];
+	strcpy(outFilePath, outFilePathPtr);
+	fd_out = open(outFilePath, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd_out == -1) {
+	   printf("Couldn't open file to output\n");
+	   exit(1);
+	}
+	dup2(fd_out, STDOUT_FILENO);
+	}
+	//-------------------------------------------
+
 	if (stat(filename, &file_info) < 0) {
-		printf("Unable to get file info"); //nao será print. mas terminr com um codigo de erro???
+		printf("Error in %d Unable to get file info", getpid()); //nao será print. mas terminr com um codigo de erro???
 		exit(1);
 	}
+	char tempFileName[512];
+	strcpy(tempFileName, filename); // guarda-se temporariamente o caminho completo, pois filename vai ser usado mais à frente novamente 
+	cleanName(filename);
+	printf("%d ", getpid());
+	printf("%s", filename);
+	strcpy(filename, tempFileName); //volta-se a repor filename com o caminho completo para o ficheiro
+	printf(",");
 	printf("%s", filetype);
 	printf(", "); 
 	printf("%ld,", file_info.st_size);
@@ -125,6 +172,14 @@ void printFileInfo(char* filename, int cripto) {
 		printf("%s", sha256result);
 		}
 		printf("\n");
+
+	// Replaces the output back to stdout
+	//----------------------------------
+	if(0){  //argument "-o" present
+		fflush(stdout);
+	  close(fd_out);
+	  dup2(1, STDOUT_FILENO);
+	}
 }
 //-----------------------------------------------------------
 
@@ -132,9 +187,9 @@ void printFileInfo(char* filename, int cripto) {
 
 //Recebe char e se for ficheiro imprime os seus stats, senao de forma recursiva percorre o diretorio
 //--------------------------------------------------------------------------------------------------
-void recurFolder(char entryFile[]){
+void recurFolder(char entryFile[], char outFilePath[], int cripto){
 
-char content[512]; //1024 possible objects in each folder each one with max 256 caracters in name
+char content[512];
 char temp[512];
 
 DIR* Dir;
@@ -143,9 +198,8 @@ struct dirent* DirEntry;
     struct stat filestat;
     stat(entryFile, &filestat); 
     
-    	
-    if(S_ISDIR(filestat.st_mode)){ //entryfFile e um diretorio
-    
+if(S_ISDIR(filestat.st_mode)){ //entryfFile e um diretorio, faz fork e processa o conteudo noutro processo
+
     Dir = opendir(entryFile);
 
     if (Dir == NULL) {
@@ -159,35 +213,39 @@ struct dirent* DirEntry;
         strcat(content, entryFile);
         strcat(content, "/");
         strcat(content, temp);
-        
         stat(content, &filestat);
 
         if(S_ISDIR(filestat.st_mode)){   //se este conteudo da pasta for diretorio, entra em recursao
           if((temp[0] != '.' && temp[1] != '.') ||   //Filtra diretorios do tipo '.' e '..'
             (temp[0] != '.' && temp[1] != '\0')) {
+
+		              pid_t pid, childPid;
+		              int status;
               
-          
-           printf("dir: %s \n", content);
-           recurFolder(content);
+		              pid = fork();
+              
+		              if(pid != 0){ //parent
+		                 childPid = wait(&status);
+		              	 if(childPid == -1){
+		              		 printf("Error parsing directory system");
+		              		 return;
+		              	 }
+		              }
+		              else{ //child
+							      recurFolder(content, outFilePath, cripto);
+									}
           }
         }
-        else{
-            printf("wFile: %s \n", content);
-            //cahma analide do ficheiro
+        else{   //chama analide do ficheiro
+          printFileInfo(content, cripto, outFilePath);            
         }
-        
-            //strcat(content, "/");
-        //strcat(content, temp);
-       
-      
     }
     closedir(Dir);
-    }
- else{    // Entryfile e um ficheiro
-    		  
-           printf("File: %s \n", entryFile);
-           //pede a analise de ficheiro
-       }
+    }		
+else{    // Entryfile e um ficheiro, pede a sua analise
+    strcpy(content, entryFile);
+    printFileInfo(content, cripto, outFilePath); 
+ }
 }
 
 
@@ -205,16 +263,9 @@ int main(int argc, char* argv[], char* envp[]) {
 					arguments[i-1] = argv[i];
                 }
 		}*/
-
+char outFilePath[] = "./outFile.csv";
 char entryFile[] = "folder";
-//int cripto = 3; //number of criptographic hashes asked
-
-
-/*int count = 0;
-char entryFile[1];
-strcat(entryFile, "./");
-strcat(entryFile[count], arguments[argc-1]);*/
-
+int cripto = 3; //number of criptographic hashes asked
 
 /*
  //ENVIROMENT VARIABLES
@@ -235,28 +286,16 @@ strcat(entryFile[count], arguments[argc-1]);*/
 */
 
 
-recurFolder(entryFile);
+recurFolder(entryFile, outFilePath, cripto);
 
 
 /*-------------------------------------------------
 	Process the file and produce the required output
 	------------------------------------------------
-	
-	
-	char outFilePath[80] = "./outFile.csv";
-	int fd_out;
 
-	if (0) { //argument "-o" present
-	fd_out = open(outFilePath, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	if (fd_out == -1) {
-	printf("Couldn't open file to output\n");
-	return 1;
-	}
-	dup2(fd_out, STDOUT_FILENO);
-	printFileInfo(argv[1], cripto);
-	fflush(stdout);
-	close(fd_out);
-	dup2(1, STDOUT_FILENO);
+	
+
+	
 	}
 	else {
 	printFileInfo(argv[1], cripto);
