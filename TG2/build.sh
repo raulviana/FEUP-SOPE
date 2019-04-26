b7855238a1146d805cbc40424a94f9e00ef8da59
Raul Viana <raulmanuelviana@gmail.com>
2019-04-26 11:40:45 +0100

    Working base organized and ready

---
+++ TG2/build.sh
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env bash
+
+cd server_src
+make clean; make
+mv server ..
+
+cd ../client_src
+make clean; make
+mv client ..
---
+++ TG2/client_src/client.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+#define FIFO_TIMEOUT_SECS  30 
+
+int main(int argc, char*argv[]){
+
+     printf("client \n");
+
+     return 0;
+}
