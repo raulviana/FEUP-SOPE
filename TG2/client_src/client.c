f189c883ad8d9679756347b6c8029fe163ec4d19
raulviana <raulmanuelviana@gmail.com>
2019-04-29 12:43:29 +0100

    started making fifos and trying to send message

--- TG2/client_src/client.c
+++ TG2/client_src/client.c
@@ -3,10 +3,18 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <errno.h>
 #include "../constants.h"
 #include "../types.h"
 
 #define ERR_ARGS 2
+#define ERR_FIFO 3
+#define SERVER_FIFO_ERROR 4
+#define MESSAGE_SENT_ERROR 5
+
 #define MAX_LINE_LENGTH 512
 
 typedef unsigned int u_int;
@@ -16,9 +24,32 @@ void print_usage(FILE* stream) {
     fprintf(stream, "Usage: ./client <Account ID> <\"Password\"> <Operation Delay (ms)> <Operation Code> <\"Operation Arguments\">\n");
 }
 
+void send_message(tlv_request_t message){
+    int fd;
+    if ((fd=open(SERVER_FIFO_PATH, O_WRONLY | O_CREAT | O_APPEND, 0660)) < 0){
+        printf("Failed to open server requests FIFO\n");
+        exit(SERVER_FIFO_ERROR);
+    }
+    int write_result = write(fd, &message, sizeof(message));
+    if(write_result < 0){
+        printf("Error: Failed to send message to server FIFO\n");
+        exit(MESSAGE_SENT_ERROR);
+    }
+}
+/*
+void getWidth(char *arg[], int width){
+    
+    strcat(account_ID, argv[1]);
+    while((strlen(account_final) + strlen(account_ID)) <= WIDTH_ACCOUNT - 1){
+        strcat(account_final, "0");
+    }
+    strcat(account_final, account_ID);
+} */ 
+
 
 
 int main(int argc, char*argv[]){
+
 
     if (argc != 6) {
         print_usage(stderr);
@@ -28,6 +59,17 @@ int main(int argc, char*argv[]){
     //************Prepare arguments*******************
 
     req_header_t header;
+    req_create_account_t createAccount;
+    req_transfer_t transfer;
+    req_value_t header_args;
+    tlv_request_t message_send;
+
+    //PID
+    header.pid = getpid();
+    char strPid[WIDTH_ID];
+    sprintf(strPid, "%d", getpid());
+     printf("%s\n", strPid);
+   
     //Account ID
     u_int account_number = atoi(argv[1]);
     if (account_number < 1 || account_number > MAX_BANK_ACCOUNTS){
@@ -47,17 +89,31 @@ int main(int argc, char*argv[]){
 
     //Delay
     u_int delay = atoi(argv[3]);
-    //não de faz qualqeur tipo de verificacao ao delay?????
+    if(delay > MAX_OP_DELAY_MS || delay <= 0){
+        printf("Error: Invalid delay. Use delay between 0 and 99999.\n");
+        print_usage(stderr);
+        exit(ERR_ARGS);
+    }
     header.op_delay_ms = delay;
     
-    //Operation Code
+
+    /*Operation Code
+    * 0 - Account creation: "AccountID balance "password"" 
+    * 1 - Balance consultation: no extra arguments
+    * 2 - Between Accounts Transfer: "destinationAccountID value"
+    * 3 - Server ShutDown: no extra arguments
+    */
     u_int op_code = atoi(argv[4]);
-    if(op_code < 0 || op_code > 3){
+    if(op_code < 0 || op_code >= __OP_MAX_NUMBER){
         printf("Invalid Operation Code. Use 0, 1, 2 or 3.\n");
         print_usage(stderr);
         exit(ERR_ARGS);
-    }                                                                 //op_code has operation code verified
-    //struct header finished
+    }
+    //struct header is finished
+
+    if(op_code == OP_BALANCE || op_code ==  OP_TRANSFER){
+        header_args.header = header;
+    }
 
 
     //Operation Arguments
@@ -72,11 +128,10 @@ int main(int argc, char*argv[]){
        ptr = strtok(NULL, " ");
        count++;
     }
+    //operation arguments list is ready
 
-    //operation arguments list ready
 
-    req_create_account_t createAccount;
-    if(op_code == 0){
+    if(op_code == OP_CREATE_ACCOUNT){
         
         if(atoi(argList[0]) < 1 || atoi(argList[0]) > MAX_BANK_ACCOUNTS){
             printf("Invalid Destination Account ID. Use: 1 <= Account ID <= 4096\n");
@@ -97,13 +152,13 @@ int main(int argc, char*argv[]){
             createAccount.account_id = atoi(argList[0]);
             createAccount.balance = atoi(argList[1]);
             strcpy(createAccount.password, argList[2]);
-        }        
+            header_args.create = createAccount;
+        }    
+
     }
-    //createAccount struct ready
+    //createAccount struct is ready
 
-
-    req_transfer_t transfer;
-    if(op_code == 2){
+    if(op_code == OP_TRANSFER){
         if(atoi(argList[0]) < 1 || atoi(argList[0]) > MAX_BANK_ACCOUNTS){
             printf("Invalid Destination Account ID. Use: 1 <= Account ID <= 4096\n");
             print_usage(stderr);
@@ -117,14 +172,33 @@ int main(int argc, char*argv[]){
         else{
             transfer.account_id = atoi(argList[0]);
             transfer.amount = atoi(argList[1]);
+            header_args.transfer = transfer;
         }
     }
     // transfer struct ready
+    
+    message_send.value = header_args;
+    message_send.type = op_code;
+    message_send.length = sizeof(header_args);
+    //message TLV is ready to be sent!
+
     //*************************************************
 
 
+//****************Server fifo **********************
+    if (mkfifo(SERVER_FIFO_PATH,0660)<0)
+ if (errno==EEXIST) printf("FIFO '/tmp/requests' already exists\n");
+ else printf("Can't create FIFO\n");
+else printf("FIFO '/tmp/requests' sucessfully created\n");
+send_message(message_send);
+ if (unlink(SERVER_FIFO_PATH)<0)
+ printf("Error when destroying FIFO '/tmp/requests'\n");
+ else
+ printf("FIFO '/tmp/requests' has been destroyed\n"); 
+//*********************************************
 
-    
+
+
     return 0;
 }
 
